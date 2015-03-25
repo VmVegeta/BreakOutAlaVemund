95eea87cd74d327ea98ca322f2dcd08cd7f17b7d
vmvegeta <vemund1994@hotmail.com>
2015-03-25 14:40:36 +0100

    level2 bricks

--- Breakout/src/Level.java
+++ Breakout/src/Level.java
@@ -16,11 +16,7 @@ public class Level extends Pane{
 		setLevel1();
 		getChildren().add(racket);
 		getChildren().add(ball);
-		for(Brick brick: bricks){
-			if(brick.isDestroyed() == false){
-				getChildren().add(brick);
-			}
-		}
+		
 		inintTimeline();
 	}
 
@@ -54,6 +50,11 @@ public class Level extends Pane{
 				bricks.add(new Brick(50 + j * 47, 60 + i * 22));
 			}
 		}
+		for(Brick brick: bricks){
+			if(brick.isDestroyed() == false){
+				getChildren().add(brick);
+			}
+		}
 
 	}
 	private void keyPressed() {
@@ -81,38 +82,35 @@ public class Level extends Pane{
 	private void checkCollisionWithBrick(){
 		for(int i = 0; i < bricks.size(); i++){
 			if(bricks.get(i).isDestroyed() == true){
+				getChildren().remove(bricks.get(i));
 				bricks.remove(i);
 			}
 		}
 		for(int i = 0; i < bricks.size(); i++){
-			if((ball.getX() + 3) >= bricks.get(i).getTranslateX() && (ball.getX() -3) <= (bricks.get(i).getTranslateX() + bricks.get(i).getWidth())
+			if((ball.getX() + 6) >= bricks.get(i).getTranslateX() && (ball.getX() -6) <= (bricks.get(i).getTranslateX() + bricks.get(i).getWidth())
 					&& ball.getTop() <= (bricks.get(i).getTranslateY() + bricks.get(i).getHeight() +1) 
 					&& ball.getTop() >= (bricks.get(i).getTranslateY() + bricks.get(i).getHeight() -2)){
 
 				bricks.get(i).setDestroyed(true);
-				getChildren().remove(bricks.get(i));
 				ball.setBallDirectionY(3);
 
-			}else if((ball.getX() + 3) >= bricks.get(i).getTranslateX() && (ball.getX() - 3) <= (bricks.get(i).getTranslateX() + bricks.get(i).getWidth())
+			}else if((ball.getX() + 6) >= bricks.get(i).getTranslateX() && (ball.getX() - 6) <= (bricks.get(i).getTranslateX() + bricks.get(i).getWidth())
 					&& ball.getBottom() <= (bricks.get(i).getTranslateY() +1) && ball.getBottom() >= (bricks.get(i).getTranslateY() -2)){
 
 				bricks.get(i).setDestroyed(true);
-				getChildren().remove(bricks.get(i));
 				ball.setBallDirectionY(-3);
 
-			}else if((ball.getY() + 3) >= bricks.get(i).getTranslateY() && (ball.getY() - 3) <= (bricks.get(i).getTranslateY() + bricks.get(i).getHeight())
+			}else if((ball.getY() + 6) >= bricks.get(i).getTranslateY() && (ball.getY() - 6) <= (bricks.get(i).getTranslateY() + bricks.get(i).getHeight())
 					&& ball.getRigth() <= (bricks.get(i).getTranslateX() + 1) && ball.getRigth() >= (bricks.get(i).getTranslateX() - 2)){
 
 				bricks.get(i).setDestroyed(true);
-				getChildren().remove(bricks.get(i));
 				ball.setBallDirectionX(-3);
 
-			}else if((ball.getY() + 3) >= bricks.get(i).getTranslateY() && (ball.getY() - 3) <= (bricks.get(i).getTranslateY() + bricks.get(i).getHeight())
+			}else if((ball.getY() + 6) >= bricks.get(i).getTranslateY() && (ball.getY() - 6) <= (bricks.get(i).getTranslateY() + bricks.get(i).getHeight())
 					&& ball.getLeft() <= (bricks.get(i).getTranslateX() + bricks.get(i).getWidth() + 1)
 					&& ball.getLeft() >= (bricks.get(i).getTranslateX() + bricks.get(i).getWidth() - 2)){
 
 				bricks.get(i).setDestroyed(true);
-				getChildren().remove(bricks.get(i));
 				ball.setBallDirectionX(3);
 
 			}	
